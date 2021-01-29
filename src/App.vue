bcf9808e233086ce4f9a618a240d6d3b8167713d
infodo <hoferdominik.02@gmail.com>
2021-01-29 16:55:47 +0100

    finished screen design

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <!--Header-->
+    <div>
+      <div class="flex flex-col items-center">
+        <div
+          class="bg-st-img flex-col h-40 w-full bg-cover flex justify-center items-center"
+        >
+          <img src="/src/assets/st-logo-white.png" class="w-60" alt="" />
+        </div>
+
+        <h1 class="text-2xl text-black font-sp text-center mt-6 text-primary">
+          Tourismus Dashboard mit dem Open Data Hub
+        </h1>
+      </div>
+    </div>
+    
+    <!-- Divider -->
+    <div>
+      <h1 class="font-sp text-2xl text-center text-secondary-500">....</h1>
+    </div>
+
+    <!-- Hotelblock -->
+    <div>
+      <h2 class="text-secondary-300 font-bold text-2xl text-center my-5">Hotels in Südtirol</h2>
+      <div class="flex flex-col items-center justify-center lg:flex-row flex-wrap" >
+      <div >
+        <div class="flex flex-col items-center mb-8 mx-7" >
+        <p class="font-sp text-8xl text-secondary-400">12%</p>
+        <p class="text-sgray" >Hotelauslastung in Prozent</p>
+      </div>
+      </div>
+      <div>
+        <div class="flex flex-col items-center mb-8 mx-7" >
+        <p class="font-sp text-8xl text-secondary-400">10.300</p>
+        <p class="text-sgray" >Belegte Zimmer</p>
+      </div>
+      </div>
+      <div>
+        <div class="flex flex-col items-center mb-8 mx-7" >
+        <p class="font-sp text-8xl text-secondary-400">56.083</p>
+        <p class="text-sgray" > Freie Betten</p>
+      </div>
+      </div>
+    </div>
+    </div>
+
+    <!-- Divider -->
+    <div>
+      <h1 class="font-sp text-2xl text-center text-secondary-500">....</h1>
+    </div>
+
+    <!-- Gastro Block -->
+    
+     <div>
+      <h2 class="text-secondary-300 font-bold text-2xl text-center my-5">Gastronomie in Südtirol</h2>
+      <div class="flex flex-col items-center justify-center lg:flex-row flex-wrap" >
+      <div >
+        <div class="flex flex-col items-center mb-8 mx-7" >
+        <p class="font-sp text-8xl text-secondary-400">12.036</p>
+        <p class="text-sgray" >Ausgegebene Speisen in den letzten 24 Stunden</p>
+      </div>
+      </div>
+      <div>
+        <div class="flex flex-col items-center mb-8 mx-7" >
+        <p class="font-sp text-8xl text-secondary-400">10.300</p>
+        <p class="text-sgray" >Restaurants und </p>
+      </div>
+      </div>
+      <div>
+        <div class="flex flex-col items-center mb-8 mx-7" >
+        <p class="font-sp text-8xl text-secondary-400">56.083</p>
+        <p class="text-sgray" > Freie Betten</p>
+      </div>
+      </div>
+    </div>
+    </div>
+
+    
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sgray: '#6d818e',
+        primary: '#710027',
+        secondary: {
+          100: '#c1cd00',
+          200: '#5d6d21',
+          300: '#ffc746',
+          400: '#ef7e40',
+          500: '#c93948',
+          600: '#b1ddf1',
+        },
+        
+      },
+      fontFamily: {
+        sp: ['Suedtirol Pro']
+      },
+      backgroundImage: theme => ({
+        'st-img': "url('/src/assets/st-img-bg.jpg')",
+       })
+    },
   },
   variants: {
     extend: {},
