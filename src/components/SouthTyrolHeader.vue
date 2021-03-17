0169ca6c5887bec23d741c3b859144231fbe10ba
infodo <hoferdominik.02@gmail.com>
2021-03-17 16:51:15 +0100

    costumized header image

--- src/components/SouthTyrolHeader.vue
+++ src/components/SouthTyrolHeader.vue
@@ -2,12 +2,12 @@
     <div>
       <div class="flex flex-col items-center">
         <div
-          class="bg-st-img flex-col h-40 w-full bg-cover flex justify-center items-center"
+          class="bg-st-img flex-col h-40 lg:h-80 w-full bg-cover flex justify-center items-center"
         >
           <img src="/src/assets/st-logo-white.png" class="w-60" alt="" />
         </div>
 
-        <h1 class="text-2xl text-black font-sp text-center mt-6 text-primary">
+        <h1 class="text-2xl font-sp text-center mt-6 text-primary">
           Hotel Dashboard mit dem Open Data Hub
         </h1>
       </div>
