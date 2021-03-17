777014071a98a77c9aaec972aae76e8832bae7d3
infodo <hoferdominik.02@gmail.com>
2021-03-17 22:29:57 +0100

    designed the hotel list

--- src/view/Hotel.vue
+++ src/view/Hotel.vue
@@ -10,50 +10,82 @@
     <!-- Hotelblock -->
     <div>
       <div>
-          <div class="flex flex-col items-center mb-16 mt-4 mx-7">
-            <div class="flex flex-row items-center">
-              <div >
-              <svg class="h-7 text-secondary-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-</svg>
+        <div class="flex flex-col items-center mb-16 mt-4 mx-7">
+          <div class="flex flex-row items-center">
+            <div>
+              <svg
+                class="h-7 text-secondary-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+              </svg>
             </div>
-            <div >
-              <svg class="h-7 text-secondary-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-</svg>
+            <div>
+              <svg
+                class="h-7 text-secondary-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+              </svg>
             </div>
-            <div >
-              <svg class="h-7 text-secondary-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-</svg>
+            <div>
+              <svg
+                class="h-7 text-secondary-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+              </svg>
             </div>
-            
 
             <div>
               <p class="text-secondary-300 font-bold text-xl ml-1">S</p>
             </div>
-            </div>
+          </div>
 
-            <p class="font-sp text-4xl mx-5 md:text-6xl lg:text-9xl text-center text-secondary-200">St. Hotelname Welness und Spa mit Dusche</p>
-            <h2 class="text-secondary-100 font-bold text-2xl text-center mt-3"> dein Hotel in TollerOrt
-      </h2>
-      <div class="flex flex-row items-center mt-2 hover:border-secondary-100 border-b-2  transition duration-300 ease-linear ">
-        <p class="font-medium text-secondary-200 mr-4">Hotelwebseite</p>
-        <svg class="h-5 text-secondary-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-</svg>
-      </div>
+          <p
+            class="font-sp text-4xl mx-5 md:text-6xl lg:text-9xl text-center text-secondary-200"
+          >
+            St. Hotelname Welness und Spa mit Dusche
+          </p>
+          <h2 class="text-secondary-100 font-bold text-2xl text-center mt-3">
+            dein Hotel in TollerOrt
+          </h2>
+          <div
+            class="flex flex-row items-center mt-2 hover:border-secondary-100 border-b-2 transition duration-300 ease-linear"
+          >
+            <p class="font-medium text-secondary-200 mr-4">Hotelwebseite</p>
+            <svg
+              class="h-5 text-secondary-200"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
           </div>
         </div>
+      </div>
 
-
-
-        
-        
-        <h2 class="text-sgray font-bold text-2xl text-center mb-6 mt-24">Austattung
+      <h2 class="text-sgray font-bold text-2xl text-center mb-6 mt-24">
+        Austattung
       </h2>
-     
-       
+
       <div
         class="flex flex-col items-center justify-center lg:flex-row flex-wrap"
       >
@@ -79,10 +111,7 @@
     </div>
 
     <!-- Link -->
-    <div>
-      
-    </div>
-
+    <div></div>
   </div>
 </template>
 
--- src/view/List.vue
+++ src/view/List.vue
@@ -30,43 +30,132 @@
 
         <!-- List -->
         <div>
-        <div class="flex flex-col items-center mb-16 mt-4 mx-7">
-          <p class="font-sp text-4xl lg:text-6xl text-secondary-200">6.508</p>
-          <h2 class="text-secondary-100 font-bold text-2xl text-center mt-3">
-            Hotels <span class="text-secondary-200">mit Sauna</span> für dich in
-            Südtirol
-          </h2>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 m-5">        
+
+         
+         
+          <div class="bg-gray-200 bg-opacity-25 w-full  p-4 shadow-lg rounded-2xl flex flex-col items-center overflow-hidden">
+            <div>
+
+              <div class="flex flex-row items-center">
+            <div>
+              <svg
+                class="h-6 text-secondary-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+              </svg>
+            </div>
+            <div>
+              <svg
+                class="h-6 text-secondary-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+              </svg>
+            </div>
+            <div>
+              <svg
+                class="h-6 text-secondary-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+              </svg>
+            </div>
+
+            <div>
+              <p class="text-secondary-300 font-bold text-lg ml-1">S</p>
+            </div>
+          </div>
+
+            </div>
+            <h3 class="text-secondary-400 font-sp text-xl text-center" >
+              Hotel mit Meerblicjk ugcftxrfh hvgvgvhzbub hbhv 
+            </h3>
+
+            <div class="flex">
+              <p class="text-sgray font-medium">
+              ORT
+            </p>
+            <p class="text-sgray font-bold mx-2">
+              ·
+            </p>
+              <p class="text-sgray font-medium">
+              Halbpension
+            </p>
+
+            </div>
+
+            <p class="text-sgray font-light">
+              Ausgebucht
+            </p>
+
+
+
+            <div  class="my-2  w-full border-t-2 border-b-2 py-2">
+
+              <div class="mx-12 flex flex-row justify-between"> <p class="font-medium text-secondary-500" >Fenster</p> <p class="font-light text-sgray">Nein</p></div>
+              <div class="border-b-2 mx-8 my-2"> </div>
+              <div class="mx-12 flex flex-row justify-between"> <p class="font-medium text-secondary-500" >Fenster</p> <p class="font-light text-sgray">Nein</p></div>
+
+              
+            </div>
+
+            
+
+            <div
+            class="flex flex-row items-center mt-2 hover:border-secondary-300 border-b-2 transition duration-300 ease-linear"
+          >
+            <p class="font-medium text-secondary-400 mr-4">Zur Hotelübersicht</p>
+            <svg
+              class="h-5 text-secondary-400"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+
+
+
+          </div>
+
+         
+         
+
+         
+
         </div>
+
       </div>
 
-      
+      <div class="flex items-center flex-col w-full">
+        <p class="text-secondary-500 text-center font-sp text-6xl mb-6 mt-12">
+          Urlaub im Herzen Europas
+        </p>
 
-      <h2 class="text-sgray font-bold text-2xl text-center mb-6 mt-24">
-        Aktuelle Auslastung
-      </h2>
+        <img src="../assets/st-berge.png" alt="">
 
-      <div
-        class="flex flex-col items-center justify-center lg:flex-row flex-wrap"
-      >
-        <div>
-          <div class="flex flex-col items-center mb-8 mx-7">
-            <p class="font-sp text-6xl text-secondary-600">12%</p>
-            <p class="text-sgray font-medium">Hotelauslastung in Prozent</p>
-          </div>
-        </div>
-        <div>
-          <div class="flex flex-col items-center mb-8 mx-7">
-            <p class="font-sp text-6xl text-secondary-600">10.300</p>
-            <p class="text-sgray font-medium">Belegte Zimmer</p>
-          </div>
-        </div>
-        <div>
-          <div class="flex flex-col items-center mb-8 mx-7">
-            <p class="font-sp text-6xl text-secondary-600">56.083</p>
-            <p class="text-sgray font-medium">Freie Betten</p>
-          </div>
-        </div>
       </div>
+     
     </div>
 
     <!-- Link -->
